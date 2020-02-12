cf845e34b8dabb6e61ad341dc98864f537549f4f
J03lqm <60689513+J03lqm@users.noreply.github.com>
2020-02-12 14:54:36 +0100

    Update code.js

--- code.js
+++ code.js
@@ -4,7 +4,7 @@
 /////////////////
 
 /**
- * Generar
+ * generar
  * Receives code as parameter and converts it into binary 
  * @param {String} intrucciones - Code that is going to be converted into binary instructions
  * @return {String} Binary code
