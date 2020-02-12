614ad064bac6976bb09388b8440b2f4611956912
J03lqm <60689513+J03lqm@users.noreply.github.com>
2020-02-12 14:54:01 +0100

    Add files via upload

--- code.js
+++ code.js
@@ -4,22 +4,26 @@
 /////////////////
 
 /**
- * 
- * @param {*} intrucciones 
+ * Generar
+ * Receives code as parameter and converts it into binary 
+ * @param {String} intrucciones - Code that is going to be converted into binary instructions
+ * @return {String} Binary code
  */
-function Generar(intrucciones) {
+function generar(intrucciones) {
   if (intrucciones != "") {
+    // Array that contains the instructions
     let arrInstrucciones = [];
+    // String where binary code is going to be written
     let maquina = "";
     arrInstrucciones = intrucciones.split(/ |\n/);
     for (let x = 0; x < arrInstrucciones.length; x++) {
-      //Calcula el valor
+      // Calculates value in binary
       if (isNaN(arrInstrucciones[x]) === false) {
         let valor = calcularBinario(arrInstrucciones[x]);
         maquina += valor;
       }
 
-      //instrucciones
+      // Instructions
       else if (arrInstrucciones[x] == "imprime") {
         maquina += "00000";
       }
@@ -44,7 +48,7 @@ function Generar(intrucciones) {
       else if (arrInstrucciones[x] == "saltasi0") {
         maquina += "00111";
       }
-      // Registros
+      // Registeries
       else if (arrInstrucciones[x] == "R0") {
         maquina += "000";
       }
@@ -69,24 +73,25 @@ function Generar(intrucciones) {
       else if (arrInstrucciones[x] == "R7") {
         maquina += "111";
       }
+      // Write "00000000" as value in instructions that doesnt request it
       if ((arrInstrucciones[x] == "R0" || arrInstrucciones[x] == "R1" || arrInstrucciones[x] == "R2" || arrInstrucciones[x] == "R3" || arrInstrucciones[x] == "R4" || arrInstrucciones[x] == "R5" || arrInstrucciones[x] == "R6" || arrInstrucciones[x] == "R7") && isNaN(arrInstrucciones[x + 1]) === true) {
         maquina += "00000000";
       }
     }
     txtResultado.innerHTML = maquina;
+  return maquina;
   }
-  else
-    return maquina;
 }
 
 /**
  * Converts a decimal number into binary number
- * @param {num} num Decimal number which is going to be converted
- * @returns {num} num converted into binary
+ * @param {num} num - Decimal number which is going to be converted
+ * @return {num} num converted into binary
  */
 function calcularBinario(num) {
-  // 
+  // Calculated binary number (reverse)
   let cadena = "";
+  // Final binary number
   let cadenaInvertida = "";
   while (num != 0 && num != 1) {
     cadena += num % 2;
@@ -95,10 +100,11 @@ function calcularBinario(num) {
   if (num == 1) {
     cadena += "1";
   }
+// Ensure binary number length is 8
   for (let i = cadena.length; i < 8; i++) {
     cadena += "0";
   }
-
+// Reverts the string
   for (let x = cadena.length; x >= 0; x--) {
     cadenaInvertida += cadena.charAt(x);
   }
@@ -106,17 +112,21 @@ function calcularBinario(num) {
 }
 
 /**
- *
- *
- * @param {*} maquina
+ * Excutes the binary code and write its result in txtEjecucion
+ * @param {String} maquina - Binary code that is going to be executed
+ * registroDec {String} - Contains the decimal registry
+ * valorDec {String} - Contains the decimal value
  */
 function ejecutar(maquina) {
   let arrFunciones = maquina.match(/.{1,16}/g);
   let arrRegistro = [0, 0, 0, 0, 0, 0, 0, 0];
   txtEjecucion.innerHTML="";
   for (let x = 0; x < arrFunciones.length; x++) {
+    // String that contain JUST the instruction
     let instruccion = arrFunciones[x].slice(0, 5);
+    // String that contains JUST the registry
     let registroBin = arrFunciones[x].slice(5, 8);
+    // String that contains JUST the value
     let valorBin = arrFunciones[x].slice(8, 16);
 
     // Instrucciones
@@ -128,6 +138,7 @@ function ejecutar(maquina) {
     // imprimec
     else if(instruccion == "00001"){
       let registroDec = calcularDecimal(registroBin);
+      // Receives the character in Unicode
       let caracter=String.fromCharCode(arrRegistro[registroDec]);
       txtEjecucion.innerHTML+=caracter;
     }
@@ -172,9 +183,14 @@ function ejecutar(maquina) {
     }
   }
 }
-
+/**
+ * Converts a binary number into decimal
+ * @param {String} bin - Binary number which is going to be converted
+ * @returns {number} bin in decimal
+ */
 function calcularDecimal(bin) {
   let decimal=0;
+  // Array that contains every digit in bin
   let arrBin=bin.split("");
   let binReverse = arrBin.reverse().join("");
   for(let x=0;x<binReverse.length;x++){
@@ -195,5 +211,5 @@ function calcularDecimal(bin) {
 let txtIntrucciones = document.querySelector("#txtIntrucciones");
 let txtResultado = document.querySelector("#txtResultado");
 let txtEjecucion = document.querySelector("#txtEjecucion");
-document.querySelector("#btnGenerar").addEventListener("click", function () { Generar(txtIntrucciones.value) });
+document.querySelector("#btnGenerar").addEventListener("click", function () { generar(txtIntrucciones.value) });
 document.querySelector("#btnEjecutar").addEventListener("click", function () { ejecutar(txtResultado.value) });
